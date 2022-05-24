09310a4cd24c1da002e17c2705234d5dab3446f2
Rebecca Sissman <rsissman15@aol.com>
2022-05-24 10:55:27 -0400

    commit tree d7d2b5858e5d66230831ec730d510d2b0102914a

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+
+    attr_reader :knowledge
+   
+    def initialize 
+        @knowledge = []
+    end
+
+    def learn (str)
+        self.knowledge << str 
+    end
+
 
 end
